8c7351227f262191eba043a8b67bcfcf20237921
BrunaNath <133688431+BrunaNath@users.noreply.github.com>
2023-05-25 16:56:08 -0300

    Java script
    aprendemos a criar códigos para o Java script

---
+++ ex004/scripts/meuprimeiroscript.js
@@ -0,0 +1,12 @@
+window.alert('Seja bem vindo ao meu site!')
+window.confirm('Vamos confirmar seus dados?')
+window.prompt('Qual é seu nome')
+console.log('Olá Seja Bem Vindo a nossa aula JavaScript')
+/* Voces podem usar aspas simples, aspas duplas ou acento grave */
+
+var nome = window.prompt('Qual é seu nome?')
+/* recebe */
+/* Esse sinal = não significa igual no Javascript Aqui o sinal de igualdade é assim, ==. */
+
+window.alert(`Seja bem vindo! ${nome}`)
+console.log(`Seja bem vindo! ${nome}`)
